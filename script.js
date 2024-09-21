ead0addf9aadb22cf7462d7fc5c0874b678dec28
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-09-21 16:29:36 +0530

    Creating a Weather Data API

---
+++ script.js
@@ -0,0 +1,8 @@
+let cityName = document.querySelector(".weather_city");
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_icon = document.querySelector(".weather_icon");
+let w_temperature= document.querySelector(".weather_temperature");
+let w_minTem = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
+
